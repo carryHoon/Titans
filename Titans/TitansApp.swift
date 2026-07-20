e86a8a43211623a2dd15e8d5d0376b2ec509aeae
Lee Seung-hoon <seunghoon003@gmail.com>
2026-07-21 08:50:42 +0900

    Initial Commit

---
+++ Titans/TitansApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitansApp.swift
+//  Titans
+//
+//  Created by MacH on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct TitansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
